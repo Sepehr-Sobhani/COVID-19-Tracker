248f0cf863818103029812bdf6437c7e5d312257
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-11-28 19:48:18 -0800

    Add Clear button

--- vue-covid-tracker/src/views/Home.vue
+++ vue-covid-tracker/src/views/Home.vue
@@ -3,6 +3,10 @@
     <DataTitle :text="title" :dataDate="dataDate"/>
     <DataBoxes :stats="stats"/>
     <CountrySelect @get-country="getCountryData" :countries="countries"/>
+    <button v-if="stats.Country" @click="clearCountryData"
+            class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">Clear
+      Country
+    </button>
   </main>
   <main class="flex flex-col align-middle justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -42,6 +46,13 @@ export default {
     getCountryData(country) {
       this.stats = country;
       this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData()
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
